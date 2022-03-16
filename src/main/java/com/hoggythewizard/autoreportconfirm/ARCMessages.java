b2bfefa8aa99f4d5aa2836e3ecd5949d3c13d2b9
HoggyTheWizard <hogwartswizrd@icloud.com>
2022-03-16 18:19:12 -0400

    Delay command send due to Hypixel limitations and fix chat formatting

--- src/main/java/com/hoggythewizard/autoreportconfirm/ARCMessages.java
+++ src/main/java/com/hoggythewizard/autoreportconfirm/ARCMessages.java
@@ -1,25 +1,44 @@
 package com.hoggythewizard.autoreportconfirm;
 
-import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.client.Minecraft;
+import com.hoggythewizard.utils.Timer;
+import net.minecraft.util.*;
+import tv.twitch.chat.Chat;
+
+import java.lang.Thread;
 
 
 
 public class ARCMessages {
     @SubscribeEvent
     public void ClientChatReceivedEvent(ClientChatReceivedEvent event){
-        Minecraft mc = Minecraft.getMinecraft();
+        final Minecraft mc = Minecraft.getMinecraft();
         String message = event.message.getUnformattedText();
-        if (message.contains("&aPlease type &r&e/report confirm&r&e")) {
+        if (message.contains("Please type") && message.contains("/report confirm")) {
             event.setCanceled(true);
-            mc.thePlayer.sendChatMessage("/cr confirm");
+            new Thread()
+            {
+                @Override
+                public void run()
+                {
+                    Timer.waitTime(1000);
+                    mc.thePlayer.sendChatMessage("/cr confirm");
+                }
+            }.start();
         }
-        else if (message.contains("&aThanks for your Public Chat report.")) {
+        else if (message.contains("Thanks for your Public Chat report.")) {
             event.setCanceled(true);
+            ChatComponentText first = new ChatComponentText("[ARC] ");
+            first.setChatStyle(new ChatStyle().setColor(EnumChatFormatting.RED));
+            ChatComponentText second = new ChatComponentText("Successfully submitted your report.");
+            second.setChatStyle(new ChatStyle().setColor(EnumChatFormatting.GREEN));
             mc.thePlayer.addChatComponentMessage(new ChatComponentText(
-                    "&c[ARC] &aSuccessfully submitted your report."));
+                    first.getFormattedText() + second.getFormattedText()));
+        }
+        else {
+            event.setCanceled(false);
         }
     }
 }
